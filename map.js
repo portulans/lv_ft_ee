acf84a6f4b0310bc01b6e5c62acf94fbfed32aa3
portulans <solenn.tual@ensg.eu>
2026-07-15 20:09:26 +0200

    Update images 173

--- map.js
+++ map.js
@@ -2122,7 +2122,7 @@ function updatePanel(feature, markerEntry) {
 	}
 	panelRow("Traces sur le plan de 1910 ?", toYesNo(props.src_p1910));
 	panelRow("Trace sur le cadastre de 1842 ?", toYesNo(props.src_c1842));
-	if (props.panoramax !== undefined) {
+	if (props.panoramax != null && props.panoramax !== undefined && props.panoramax.trim() !== "") {
 		panelRowWithLink("Vue 360°", "Voir sur Panoramax", props.panoramax);
 	}
 
